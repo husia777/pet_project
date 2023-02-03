2b8aaf1807bf5f3dead541fe683e28986e775b5e
husein <huseinnaimov@bk.ru>
2023-02-03 11:15:18 +0500

    2

--- core/serializers.py
+++ core/serializers.py
@@ -18,8 +18,8 @@ class UserRegisterSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             first_name=validated_data['first_name'],
             last_name=validated_data["last_name"],
-            email=validated_data['email'],
-            password=validated_data['password']
+            email=validated_data['email']
         )
+        user.set_password(validated_data["password"])
         user.save()
         return user
