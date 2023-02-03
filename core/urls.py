9134da9167f456d484787fd08c6d2907b2e12bfa
husein <huseinnaimov@bk.ru>
2023-02-03 12:38:41 +0500

    2

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from core.views import UserCreateAPIView
+from core.views import UserCreateAPIView, index
 
 urlpatterns = [
+    path('index/', index),
     path('core/signup/', UserCreateAPIView.as_view()),
 ]
 
--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 
 from core.models import User
 from core.serializers import UserRegisterSerializer
 
-
+def index(request):
+    return HttpResponse('<h1>dspogdf</h1>')
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserRegisterSerializer
     queryset = User.objects.all()
