14f01c57fced30afdc9a9d0cd9067efc71dc2553
husein <huseinnaimov@bk.ru>
2023-02-03 11:33:58 +0500

    2

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from core.views import UserCreateAPIView
 
 urlpatterns = [
-    path('auth/sign-up/', UserCreateAPIView.as_view())
+    path('core/sign-up/', UserCreateAPIView.as_view())
 ]
 
