2e07bcabb68b61740513f9f25e6dc40c199ab7b9
husein <huseinnaimov@bk.ru>
2023-02-07 16:30:42 +0500

    2

--- core/serializers.py
+++ core/serializers.py
@@ -1,21 +1,43 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer
 
-from core.models import User
+from .models import User
 
 
 class SignUpSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    username = serializers.CharField(required=True, max_length=50)
+    first_name = serializers.CharField(required=False, allow_blank=True, max_length=50)
+    last_name = serializers.CharField(required=False, allow_blank=True, max_length=50)
+    email = serializers.EmailField(required=False, allow_blank=True)
+    password = serializers.CharField(required=True)
 
     def is_valid(self, raise_exception=False):
+        """Get password_repeat and remove from initial data"""
         self._password_repeat = self.initial_data.pop('password_repeat')
         return super().is_valid(raise_exception=raise_exception)
 
+    def validate_username(self, value):
+        """Ensure username doesn't exist"""
+        if self.Meta.model.objects.filter(username=value).exists():
+            raise serializers.ValidationError(['User with such username already exists'])
+        return value
+
+    def validate_password(self, value):
+        """Ensure password is valid"""
+        validate_password(value)
+        return value
+
     def validate(self, data):
+        """Ensure passwords match"""
+        print(data.get('password'))
+        print(self._password_repeat)
         if data.get('password') != self._password_repeat:
-            raise serializers.ValidationError({'password_repeat': ['Пароли не совпадают']})
+            raise serializers.ValidationError({'password_repeat': ['Passwords must match']})
         return data
 
     def create(self, validated_data):
+        """Create user"""
         user = User.objects.create(**validated_data)
         user.set_password(user.password)
         user.save()
@@ -24,5 +46,3 @@ class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
-
-
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from .models import User
+from .serializers import SignUpSerializer
 
-# Create your views here.
+
+class SignUpView(CreateAPIView):
+    """Create new user"""
+    queryset = User.objects.all()
+    serializer_class = SignUpSerializer
