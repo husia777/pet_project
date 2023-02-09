eb1cfc75200eb8aa1a6e5b1a8cf8f43a8911c485
husein <huseinnaimov@bk.ru>
2023-02-09 13:20:36 +0500

    2

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'postgres',
         'PORT': '5432',
     }
 }
