de66be000c1956e744a35d2702a71a4e36865357
husein <huseinnaimov@bk.ru>
2023-02-09 17:36:42 +0500

    2

--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import login, authenticate, logout
-from rest_framework import status
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST, HTTP_200_OK
 
 from .models import User
 from .serializers import SignUpSerializer, LoginSerializer, RetrieveUpdateSerializer, PasswordUpdateSerializer
@@ -20,7 +20,7 @@ class LoginAPIView(CreateAPIView):
         serializer = self.get_serializer(data=request.data)
 
         if not serializer.is_valid():
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data=serializer.errors, status=HTTP_400_BAD_REQUEST)
 
         username = request.data.get('username')
         password = request.data.get('password')
@@ -29,9 +29,9 @@ class LoginAPIView(CreateAPIView):
         # login
         if user:
             login(request, user)
-            return Response(status=status.HTTP_200_OK)
+            return Response(status=HTTP_200_OK)
 
-        return Response(data={'password': ['Invalid password']}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(data={'password': ['Invalid password']}, status=HTTP_400_BAD_REQUEST)
 
 class UserRetrieveUpdateView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
@@ -43,7 +43,7 @@ class UserRetrieveUpdateView(RetrieveUpdateDestroyAPIView):
 
     def delete(self, request, *args, **kwargs):
         logout(request)
-        return Response(status=status.HTTP_200_OK)
+        return Response(status=HTTP_200_OK)
 
 
 class PasswordUpdateView(UpdateAPIView):
@@ -60,9 +60,9 @@ class PasswordUpdateView(UpdateAPIView):
 
         if serializer.is_valid():
             if not self.object.check_password(serializer.data.get('old_password')):
-                return Response({"old_password": ["Wrong password passed, try again."]}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"old_password": ["Wrong password passed, try again."]}, status=HTTP_400_BAD_REQUEST)
             self.object.set_password(serializer.data.get('new_password'))
             self.object.save()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(status=HTTP_204_NO_CONTENT)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
