d46eb3e8a8bb2ee36eda5735053f4cb8448483da
husia777 <huseinnaimov72@gmail.com>
2023-05-06 23:14:27 +0500

    1

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends, Form, status
+from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm  # Это форма регистрации и авторизации
 from auth.schemas import Token, UserCreate, User, BaseUser, UserUpdate
 from auth.services import AuthService, get_current_user
-from fastapi.responses import Response, RedirectResponse
+from fastapi.responses import Response
 from starlette.status import HTTP_204_NO_CONTENT
 router = APIRouter(
     prefix='/core',
@@ -26,11 +26,12 @@ async def sign_in(
         auth_data.username,
         auth_data.password)
 
+
 @router.get("/logout/")
-async def logout(response : Response):
-  response = Response(status_code = HTTP_204_NO_CONTENT)
-  response.delete_cookie(key ='access_token')
-  return response
+async def logout():
+    response = Response(status_code=HTTP_204_NO_CONTENT)
+    response.delete_cookie(key='access_token')
+    return response
 
 
 @router.get('/profile/', response_model=User)
@@ -39,5 +40,6 @@ async def get_user(user: User = Depends(get_current_user)):
 
 
 @router.put('/profile/', response_model=User)
-async def update_user(user_data: UserUpdate, user: User = Depends(get_current_user), auth_service: AuthService = Depends()):
+async def update_user(user_data: UserUpdate, user: User = Depends(get_current_user),
+                      auth_service: AuthService = Depends()):
     return await auth_service.change_user(user_data, user)
--- src/main.py
+++ src/main.py
@@ -2,7 +2,4 @@ from fastapi import FastAPI
 from auth.router import router as auth_router
 app = FastAPI()
 
-
-app = FastAPI()
-
 app.include_router(router=auth_router)
